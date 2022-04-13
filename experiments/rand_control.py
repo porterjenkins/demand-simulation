4f2b91995f53a8d409f308a6f167a19324bedffa
Porter <porter.jenkins@deliciousai.com>
2022-04-13 14:37:56 -0600

    implement random action

--- experiments/rand_control.py
+++ experiments/rand_control.py
@@ -2,12 +2,15 @@ from sim.simulator import Simulator
 from sim import cfg
 
 from recommenders.rand_reco import RandomRecommender
+from recommenders.reco_manager import RecommendationManager
 
 displays = cfg.get_display_names()
-agents = {}
+agents = []
 
 for d in displays:
-    agents[d] = RandomRecommender(disp=d)
+    agents.append(RandomRecommender(disp=d))
+
+reco_manager = RecommendationManager(agents)
 
 sim = Simulator.build_sim()
-sim.main(agents)
+sim.main(reco_manager)
--- recommenders/rand_reco.py
+++ recommenders/rand_reco.py
@@ -5,13 +5,33 @@ from sim import cfg
 
 
 
+
 class RandomRecommender(BaseRecommender):
 
     def __init__(self, disp):
         super(RandomRecommender, self).__init__(disp)
 
     def __call__(self, state, *args, **kwargs):
+
         prods = cfg.get_product_names()
+        action = {}
+        for p in prods:
+            action[p] = 0
+
+        budget = state.n_slots
+
+        while budget > 0:
+            candidates = np.random.permutation(prods)
+            for p in candidates:
+                step = np.random.randint(0, budget+1) # high is exclusive. need to ensure loop terminates
+                action[p] += step
+                budget -= step
+                if budget == 0:
+                    break
+
+        return action
+
+"""        prods = cfg.get_product_names()
         disp = cfg.get_displays()[self.disp]
         n_slots = disp["n_slots"]
 
@@ -28,3 +48,4 @@ class RandomRecommender(BaseRecommender):
 
             free += q
 
+"""
